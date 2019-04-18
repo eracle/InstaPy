2b647c4bfccfe20f01bf3b620996aa69fa61870b
eracle <eracle@posteo.eu>
2019-04-18 17:36:35 +0200

    fixed delta_followers

--- instapy/scrapers.py
+++ instapy/scrapers.py
@@ -1,10 +1,20 @@
+import random
+import time
+
 from selenium.common.exceptions import NoSuchElementException
 
-from instapy.unfollow_util import get_users_through_dialog
-from instapy.util import web_address_navigator, is_page_available, get_relationship_counts, click_element
+from instapy.unfollow_util import scroll_to_bottom_of_followers_list, get_buttons_from_dialog, \
+    dialog_username_extractor, follow_through_dialog
+from .time_util import sleep
+from .util import click_element
+from .util import get_relationship_counts
+from .util import is_page_available
+from .util import progress_tracker
+from .util import scroll_bottom
+from .util import web_address_navigator
 
 
-def delta_followers(session, user_name, max_amount, past_followers=None):
+def delta_followers(session, user_name, max_amount=100, past_followers=None):
     """
     Given an instagram username and an optional list of past_followers, retrieves the list of new followers.
     :param session:
@@ -33,8 +43,6 @@ def delta_followers(session, user_name, max_amount, past_followers=None):
         allfollowers, allfollowing = get_relationship_counts(browser=session.browser,
                                                              username=user_name,
                                                              logger=session.logger)
-        # print(allfollowers)
-        # print(allfollowing)
 
         # skip early for no followers
         if not allfollowers:
@@ -63,23 +71,78 @@ def delta_followers(session, user_name, max_amount, past_followers=None):
             session.logger.error("`followers_link` error {}".format(str(e)))
             return []
 
-        person_list, _ = get_users_through_dialog(browser=session.browser,
-                                                  login=session.username,
-                                                  user_name=user_name,
-                                                  amount=max_amount,
-                                                  users_count=allfollowers,
-                                                  randomize=False,
-                                                  dont_include=[],
-                                                  blacklist=session.blacklist,
-                                                  follow_times=session.follow_times,
-                                                  simulation={"enabled": False, "percentage": 100},
-                                                  channel="Follow",
-                                                  jumps=session.jumps,
-                                                  logger=session.logger,
-                                                  logfolder=session.logfolder,
-                                                  past_followers=past_followers,
-                                                  wait_seconds=10,
-                                                  )
+        channel = "Follow"
+        wait_seconds = 10
+
+        sleep(1)
+
+        if max_amount > int(allfollowers * 0.85):
+            # taking 85 percent of possible amounts is
+            # a safe study
+            max_amount = int(allfollowers * 0.85)
+        try_again = 0
+        sc_rolled = 0
+
+        # find dialog box
+        # change made wrt Instapy master branch:
+        # in order to get the following method more robust to possible changes on the html
+        # I am getting all the bottom dialog webelements, and I am taking the first
+        dialog_address = "//body/div/div/div[2]"
+        dialog = session.browser.find_elements_by_xpath(dialog_address)[0]
+
+        # scroll to end of follower list to initiate first load which hides the
+        # suggestions
+        scroll_to_bottom_of_followers_list(session.browser, dialog)
+
+        buttons = get_buttons_from_dialog(dialog, channel)
+
+        abort = False
+        total_list = len(buttons)
+        simulator_counter = 0
+        start_time = time.time()
+
+        # scroll down if the generated list of user to follow is not enough to
+        # follow amount set
+        while (total_list < max_amount) and not abort:
+            before_scroll = total_list
+            for i in range(4):
+                scroll_bottom(session.browser, dialog, 2)
+                sc_rolled += 1
+                simulator_counter += 1
+                buttons = get_buttons_from_dialog(dialog, channel)
+
+                # breaking the scroll if past follower is encountered
+                partial_usernames_extracted = dialog_username_extractor(buttons)
+                for person in partial_usernames_extracted:
+                    if person in past_followers:
+                        abort = True
+
+                total_list = len(buttons)
+                progress_tracker(total_list, max_amount, start_time, session.logger)
+
+            if not abort:
+                abort = (before_scroll == total_list)
+
+            if abort:
+                if total_list < max_amount:
+                    session.logger.info("Failed to load desired amount of users!\n")
+
+            if sc_rolled > 85:  # you may want to use up to 100
+                if total_list < max_amount:
+                    # print('')
+                    session.logger.info(
+                        "Too many requests sent!  attempt: {}  |  gathered "
+                        "links: {}"
+                        "\t~sleeping a bit".format(try_again + 1, total_list))
+                    sleep(random.randint(wait_seconds, int(wait_seconds * 1.09)))
+                    try_again += 1
+                    sc_rolled = 0
+
+        print('')
+        usernames_xpath = "//*/div/div/a[@href and @title]"
+        usernames_elements = session.browser.find_elements_by_xpath(usernames_xpath)
+
+        person_list = [elem.text for elem in usernames_elements]
 
     except (TypeError, RuntimeWarning) as err:
         session.logger.error(
